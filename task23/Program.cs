030bbeeb6d82a2ca79136d8b1d1c3544e6228b01
Tamara M <milevskaya-tv@yandex.ru>
2022-09-30 20:37:30 +0300

    Выполнили задание 23

---
+++ task23/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] massivKubov = new int[N];
+int number = 1;
+int arrayArgNumber = 0;
+massivKubov[0] = 1;
+Console.WriteLine("Таблица кубов имеет вид: ");
+while(number < (N + 1) && arrayArgNumber < N)
+{
+    int kubOfNumber = number*number*number;
+    massivKubov[arrayArgNumber] = kubOfNumber;
+    Console.Write(massivKubov[arrayArgNumber].ToString() + " ");
+    number++;
+    arrayArgNumber++;
+}
