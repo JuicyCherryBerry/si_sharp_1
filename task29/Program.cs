51c08a4347ce0ef7b25b894be238aeba8040b98d
Tamara M <milevskaya-tv@yandex.ru>
2022-10-02 18:30:00 +0300

    Выполнили задание 29

---
+++ task29/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 29: Напишите программу, в которой пользователь задает длину массива, элементы которого задаются в диапазоне [1,99] и выводит на экран .
+5 -> [1, 2, 5, 7, 19]
+3 -> [6, 1, 33]
+*/
+Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+Console.Write("[");
+for(int i = 0; i < length; i++)
+{
+    array[i] = new Random().Next(1, 100);
+    if (i < length - 1)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    else
+    {
+        Console.WriteLine(array[length - 1] + "]");
+    }
+}
